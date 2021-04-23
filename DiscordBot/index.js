dfee02624947af8de88ee86bb2c03f66a490d667
Nachtwind1100011 <imbored923@gmail.com>
2021-04-23 04:24:25 -0400

    Added Discord Bot files

---
+++ DiscordBot/index.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js");
+const { Client, MessageAttachment } = require('discord.js');
+const config = require("./config.json");
+
+const client = new Discord.Client();
+
+const prefix = "!";
+
+client.on("message", function(message) { 
+
+
+    message.attachments.forEach(attachment => {
+        
+        const url = attachment.url;
+        //message.channel.send(url);
+
+        // import fs module in which writeFile function is defined. 
+        const fsLibrary  = require('fs') 
+  
+        // Data which will need to add in a file. 
+        let data = url;
+  
+        // Write data in 'newfile.txt' . 
+        fsLibrary.writeFile('C:/Users/Dominic/PycharmProjects/Domert/url.txt', data, (error) => { 
+      
+            // In case of a error throw err exception. 
+            if (error) throw err; 
+        }) 
+      });
+
+    const url = message.attachments.first().url;
+        //message.channel.send(url);
+
+        // import fs module in which writeFile function is defined. 
+    const fsLibrary  = require('fs') 
+  
+        // Data which will need to add in a file. 
+    let data = url;
+  
+        // Write data in 'newfile.txt' . 
+    fsLibrary.writeFile('C:/Users/Dominic/PycharmProjects/Domert/url.txt', data, (error) => { 
+      
+            // In case of a error throw err exception. 
+        if (error) throw err; 
+    }) 
+
+
+});                                      
+
+client.login(config.BOT_TOKEN);
